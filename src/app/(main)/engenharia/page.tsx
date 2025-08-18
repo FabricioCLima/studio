26615c49a1b8df9a8fafd4ef4f08726145c6836c
Fabricio Lima <fa.desenvolvedor2@gmail.com>
2025-08-18 16:59:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Badge variant="ou

--- src/app/(main)/engenharia/page.tsx
+++ src/app/(main)/engenharia/page.tsx
@@ -45,7 +45,7 @@ export default function EngenhariaPage() {
       return;
     }
 
-    const q = query(collection(db, 'servicos'), where('status', 'in', ['engenharia', 'agendado', 'aguardando_visita', 'em_visita', 'concluido']));
+    const q = query(collection(db, 'servicos'), where('status', 'in', ['engenharia', 'agendado', 'aguardando_visita', 'em_visita', 'concluido', 'digitacao']));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const servicesData: Service[] = [];
       querySnapshot.forEach((doc) => {
